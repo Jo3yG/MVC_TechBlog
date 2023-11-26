f7002b7ae7f0d802c9f5bb9aed8a19a6025c13a8
Jo3yG <nonuser31@yahoo.com>
2023-11-26 05:20:49 -0600

    added get to hom/contollers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,51 @@
-
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User, Comment} = require('../models');
+router.get('/', Auth, (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'createdAt'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'createdAt'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true}));
+        res.render('dashboard', {posts, loggenIn: req.session.loggedIn});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+router.get('/signup',(req, res) => {
+    res.render('signup');
+});
