3a59dbfc00890cc55e50330ac6a7b7b097e6b769
Jo3yG <nonuser31@yahoo.com>
2023-11-25 21:54:10 -0600

    added post in comments/api

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -21,3 +21,17 @@ router.get('/:id', (req,res) => {
     res.status(500).json(err);
 })
 });
+router.post('/', Auth, (req,res) => {
+    if (req.session){
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id,
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+})
+    }
+});
