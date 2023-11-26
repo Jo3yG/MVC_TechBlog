d43f2abc0f5c62cdc47b884947245d50d33f7509
Jo3yG <nonuser31@yahoo.com>
2023-11-26 03:22:09 -0600

    added post to post/api

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -84,3 +84,15 @@ router.get('/:id', (req,res) => {
     res.status(500).json(err);
 });
 });
+router.post('/', Auth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
