5408cde1f748214e261b9391f24683c79993dc48
Jo3yG <nonuser31@yahoo.com>
2023-11-26 04:09:31 -0600

    added get to user/api

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,7 +10,51 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {exclude: ['password']},
+        where:{
+            id: req.params.id
+        },
+        include:[{
+            model: Post,
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'createdAt'
+            ]
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'comment_text',
+                'createdAt'
+            ],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        },
+        {
+            model: Post,
+            attributes: ['title'],
+        }
+    ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData){
+            res.status(404).json({message: 'No User with ID provided'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
