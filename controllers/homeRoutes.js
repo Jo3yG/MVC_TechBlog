5d0377d2327c59d1fbb4723228a90fb36c6e32a1
Jo3yG <nonuser31@yahoo.com>
2023-11-26 05:25:44 -0600

    addedget to home /controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,3 +49,50 @@ router.get('/login', (req, res) => {
 router.get('/signup',(req, res) => {
     res.render('signup');
 });
+router.get('/post/:id', Auth, (req, res) => {
+    Post.findOne({
+        where:{
+            id: req.params.id,
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'createdAt'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'createdAt'
+                ],
+                include: { 
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+            {
+                model: User,
+                attributes: ['username'],
+                },
+        ],
+        
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({message: 'No post with ID provided'});
+        return;
+        }
+    const post = dbPostData.get({plain: true});
+    console.log(post);
+    res.render('single-post', {post, loggenIn: req.session.loggedIn});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
