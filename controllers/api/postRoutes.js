17fbaa43f22c2f14637fae1b9363e5c055572c0b
Jo3yG <nonuser31@yahoo.com>
2023-11-26 03:18:58 -0600

    added get to post/api

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -41,4 +41,46 @@ router.get('/', (req,res) => {
     console.log(err);
     res.status(500).json(err);
 });
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'createdAt'
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'comment_text',
+                'post_id',
+                'user_id',
+                'createdAt'
+            ],
+            inculde: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+    ]
+    })
+.then(dbPostData => {
+    if (!dbPostData){
+        res.status(404).json({message: 'No post with ID provided'});
+        return;
+    }
+    res.json(dbPostData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
