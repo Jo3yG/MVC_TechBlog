ab0a1854f3fa9eb756530dc152f14a96b8eef5e5
Jo3yG <nonuser31@yahoo.com>
2023-11-26 03:38:59 -0600

    added post to user/api

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,14 @@
 
 
-
+router.ppost('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 router.put('/:id', (req, res) => {
     User.update(req.body,{
         individualHooks: true,
