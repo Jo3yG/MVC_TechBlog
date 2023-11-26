ebb010a1330ee1f760368126ff9a8696ec87fa81
Jo3yG <nonuser31@yahoo.com>
2023-11-26 03:36:48 -0600

    added put to user/api

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,25 @@
+
+
+
+router.put('/:id', (req, res) => {
+    User.update(req.body,{
+        individualHooks: true,
+        where:{
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({message: 'No User with ID provided'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 router.delete('/id:', (req, res) => {
     User.destroy({
         where:{
